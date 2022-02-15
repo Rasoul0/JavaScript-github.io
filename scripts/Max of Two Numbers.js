2b041c862b9a36061f18cb3419a7f8c97f97b42f
Rasoul0 <87296625+Rasoul0@users.noreply.github.com>
2022-02-15 14:13:28 -0700

    update

--- scripts/Max of Two Numbers.js
+++ scripts/Max of Two Numbers.js
@@ -1,11 +1,15 @@
-console.log("You made it to Max of Two Numbers.js");
+console.log("You made it to the Max of Two Numbers.js!");
 //
+//Global Variables
 let stop = false;
 //
 function main()
 {
   let firstNumber = document.getElementById("textField1").value;
+  let secondNumber = document.getElementById("textField2").value;
+  //document.getElementById("CanBeAnything").innerHTML = alert ("You are connected");
   document.getElementById("validityTest1").innerHTML = testNaN (firstNumber);
+  document.getElementById("validityTest2").innerHTML = testNaN (secondNumber);
 }//End main()
 //
 function testNaN (number)
@@ -16,6 +20,6 @@ function testNaN (number)
     return "Type a Real Number"
   } else
   {
-    return "Input Validated"
+    return "Good. It is a number."
   }
 }//End testNaN
