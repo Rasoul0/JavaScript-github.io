5c0524db22aff42b8ef89af067226672683cac57
Rasoul0 <87296625+Rasoul0@users.noreply.github.com>
2022-03-01 14:30:58 -0700

    update

--- scripts/Landscape or Portrait.js
+++ scripts/Landscape or Portrait.js
@@ -2,11 +2,19 @@ console.log("You made it to the Landscape or Portrait.js");
 
 let stop= false;
 
+
+
+
 function main(){
 
+let firstNumber = document.getElementById("textField1").value;
+//}End main
+
+function testNaN(){
 
 
 
+}
 
 
 
