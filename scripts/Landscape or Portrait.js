bed045e1495065bfbf59c00d8472188312b63601
Rasoul0 <87296625+Rasoul0@users.noreply.github.com>
2022-03-01 14:46:33 -0700

    update

--- scripts/Landscape or Portrait.js
+++ scripts/Landscape or Portrait.js
@@ -1,22 +1,39 @@
 console.log("You made it to the Landscape or Portrait.js");
 
-let stop= false;
-
-
-
-
-function main(){
-
-let firstNumber = document.getElementById("textField1").value;
-//}End main
-
-function testNaN(){
-
-
-
+let stop = false;
+//
+function javascript(){
 }
-
-
-
-
-}
+//
+function main()
+{
+  document.getElementById("validityTest1").innerHTML = alert(testNaN(firstNumber));
+  document.getElementById("validityTest1").innerHTML = testNaN (firstNumber);
+  document.getElementById("validityTest2").innerHTML = alert(testNaN(secondNumber));
+  document.getElementById("validityTest2").innerHTML = testNaN (secondNumber);
+  if (stop == true)
+  {
+    document.getElementById("answer").innerHTML = "Please restart."
+  } else
+  {
+    document.getElementById("answer").innerHTML = "What did you say?  " + solution(firstNumber, secondNumber);
+  }//ENd stop=true
+}//End main()
+//
+function testNaN (number)
+{
+  if ( number=="" ) 
+  {
+    stop = true;
+    return "Please type a number into the textfield"
+  }//End null check
+  //
+  if ( isNaN(number) ) //Returns a Boolean
+  {
+    stop = true;
+    return "Please Type a Real Number";
+  } else
+  {
+    return "You did it!."
+  }
+}//End testNaN
