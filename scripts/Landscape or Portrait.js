ee3c5a37210d1fdc9e625896750bb12445f9c33c
Rasoul0 <87296625+Rasoul0@users.noreply.github.com>
2022-03-02 14:21:28 -0700

    Update Landscape or Portrait.js

--- scripts/Landscape or Portrait.js
+++ scripts/Landscape or Portrait.js
@@ -8,25 +8,25 @@ function javascript () { //Where colsole.log tested file goes
 //Main Function, necessary lines of code
 function main() {
 
-  document.getElementById(validityTest1).innerHTML = alert("You Made it");
-    
+  document.getElementById(validityTest1).innerHTML = alert("Welcome");
+
     let firstNumber, secondNumber; //Variables particular to JavsScript Assignment
     //Get the value of the Heighth Input Field, id="testfield1", assign it to a variable
-    firstNumber = document.getElementById("textField1").value;
-    secondNumber = document.getElementById("textField2").value;
+    firstNumber = document.getElementById("widthNumber").value;
+    secondNumber = document.getElementById("heightNumber").value;
     //Validate by alert or HTML Text in p-tag
     document.getElementById("validityTest1").innerHTML = alert (testNAN (firstNumber) );
     document.getElementById("validityTest1").innerHTML = testNAN (firstNumber);
     document.getElementById("validityTest2").innerHTML = alert (testNAN (secondNumber) );
     document.getElementById("validityTest2").innerHTML = testNAN (secondNumber);
     if (stop == true) {
-        document.getElementById("answer").innerHTML = "Restart because I said so." //Change to more appropriate message
+        document.getElementById("answer").innerHTML = "Please Restart." //Change to more appropriate message
       } else {
         //Difference between calling functions with arguements and sending to parameter's, local variables
         console.log("What did you say?", javaScriptFunction-Solution (firstNumber, secondNumber)); // Change to more appropriate message
-        document.getElementById("answer").innerHTML = "What did you say? " + geometry (firstNumber, secondNumber);
+        document.getElementById("answer").innerHTML = "Can you repeat that again please? " + geometry (firstNumber, secondNumber);
 
-      */
+
 } //End main()
 //
 function testNaN (number) {
